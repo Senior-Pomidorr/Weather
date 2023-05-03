7447b85024ca500e5956f6e190036be6e25e68a0
«Daniil <danilkulikovskiy@gmail.com>
2023-05-03 15:07:44 +0300

    finish firstVC

--- Weather/MainViewController.swift
+++ Weather/MainViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
     }
-
+    
     private let pictureView: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = UIImage(named: "background")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
@@ -38,9 +38,10 @@ class MainViewController: UIViewController {
         searchText.backgroundColor = .systemFill
         searchText.layer.cornerRadius = 5
         searchText.font = .systemFont(ofSize: 25)
+        searchText.borderStyle = .roundedRect
         let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: searchText.frame.height))
-           searchText.rightView = rightPaddingView
-           searchText.rightViewMode = .always
+        searchText.rightView = rightPaddingView
+        searchText.rightViewMode = .always
         return searchText
         
     }()
@@ -62,16 +63,67 @@ class MainViewController: UIViewController {
         return stack
     }()
     
- 
+    private let weatherState: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "sun.max")
+        image.tintColor = .black
+        return image
+    }()
+    
+    private let degrees: UILabel = {
+        let degrees = UILabel()
+        degrees.text = "21"
+        degrees.font = .systemFont(ofSize: 80, weight: .black)
+        return degrees
+    }()
+    
+    private let degreesSign: UILabel = {
+        let degrees = UILabel()
+        degrees.text = "°"
+        degrees.font = .systemFont(ofSize: 100)
+        return degrees
+    }()
+    
+    private let degreesCelsius: UILabel = {
+        let degrees = UILabel()
+        degrees.text = "C"
+        degrees.font = .systemFont(ofSize: 100)
+        return degrees
+    }()
+    
+    private let stackViewCelcius: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.spacing = 0
+        return stack
+    }()
+    
+    private let cityLabel: UILabel = {
+        let degrees = UILabel()
+        degrees.text = "London"
+        degrees.font = .systemFont(ofSize: 30)
+        degrees.translatesAutoresizingMaskIntoConstraints = false
+        return degrees
+    }()
+    
 }
 
 extension MainViewController {
     func layout() {
         view.addSubview(pictureView)
         pictureView.addSubview(stackView)
+        pictureView.addSubview(stackViewCelcius)
+        pictureView.addSubview(weatherState)
+        pictureView.addSubview(cityLabel)
         stackView.addArrangedSubview(buttonLocation)
         stackView.addArrangedSubview(searchText)
         stackView.addArrangedSubview(buttonSearch)
+        stackViewCelcius.addArrangedSubview(degrees)
+        stackViewCelcius.addArrangedSubview(degreesSign)
+        stackViewCelcius.addArrangedSubview(degreesCelsius)
         
         NSLayoutConstraint.activate([
             pictureView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -83,8 +135,19 @@ extension MainViewController {
             stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             stackView.heightAnchor.constraint(equalToConstant: 70),
-          
             
+            weatherState.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 5),
+            weatherState.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            weatherState.heightAnchor.constraint(equalToConstant: 120),
+            weatherState.widthAnchor.constraint(equalToConstant: 120),
+            
+            stackViewCelcius.topAnchor.constraint(equalTo: weatherState.bottomAnchor, constant: 5),
+            stackViewCelcius.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackViewCelcius.heightAnchor.constraint(equalToConstant: 100),
+            
+            cityLabel.topAnchor.constraint(equalTo: stackViewCelcius.bottomAnchor, constant: 5),
+            cityLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+
         ])
         
         NSLayoutConstraint.activate([
@@ -95,7 +158,7 @@ extension MainViewController {
             buttonSearch.widthAnchor.constraint(equalToConstant: 40),
             
             searchText.heightAnchor.constraint(equalToConstant: 40),
-            ])
+        ])
     }
     
 }
